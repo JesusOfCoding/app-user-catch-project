c6750750123a2868a1d3b1a8671a48352973c542
heesun <heesun9793@naver.com>
2023-04-08 19:35:15 +0900

    SID-365 TextStyle 완료

---
+++ lib/res/styles.dart
@@ -0,0 +1,180 @@
+import 'package:flutter/material.dart';
+import 'package:side/res/dimens.dart';
+import 'package:side/res/colors.dart';
+
+class TextStyles{
+ static const String fontName = 'NanumGothic';
+
+ static const TextStyle textBoldSubWhite24 = TextStyle(
+   fontSize: Dimens.font_sp24,
+   color: Colours.app_sub_white,
+   fontWeight: FontWeight.w700,
+   fontFamily: fontName,
+ );
+
+ static const TextStyle textBoldSubWhite20 = TextStyle(
+   fontSize: Dimens.font_sp20,
+   color: Colours.app_sub_white,
+   fontWeight: FontWeight.w700,
+   fontFamily: fontName,
+ );
+
+ static const TextStyle textLightSubWhite18 = TextStyle(
+   fontSize: Dimens.font_sp18,
+   color: Colours.app_sub_white,
+   fontWeight: FontWeight.w300,
+   fontFamily: fontName,
+ );
+
+ static const TextStyle textBoldSubWhite18 = TextStyle(
+   fontSize: Dimens.font_sp18,
+   color: Colours.app_sub_white,
+   fontWeight: FontWeight.w700,
+   fontFamily: fontName,
+ );
+ static const TextStyle textRegularSubWhite16 = TextStyle(
+   fontSize: Dimens.font_sp16,
+   color: Colours.app_sub_white,
+   fontWeight: FontWeight.w400,
+   fontFamily: fontName,
+ );
+
+ static const TextStyle textLightSubWhite16 = TextStyle(
+   fontSize: Dimens.font_sp16,
+   color: Colours.app_sub_white,
+   fontWeight: FontWeight.w300,
+   fontFamily: fontName,
+ );
+
+ static const TextStyle textBoldSubWhite12 = TextStyle(
+   fontSize: Dimens.font_sp12,
+   color: Colours.app_sub_white,
+   fontWeight: FontWeight.w700,
+   fontFamily: fontName,
+ );
+
+ static const TextStyle textLightSubWhite12 = TextStyle(
+   fontSize: Dimens.font_sp12,
+   color: Colours.app_sub_white,
+   fontWeight: FontWeight.w300,
+   fontFamily: fontName,
+ );
+
+ static const TextStyle textBoldSubWhite10 = TextStyle(
+   fontSize: Dimens.font_sp10,
+   color: Colours.app_sub_white,
+   fontWeight: FontWeight.w700,
+   fontFamily: fontName,
+ );
+
+ static const TextStyle textLightSubWhite10 = TextStyle(
+   fontSize: Dimens.font_sp10,
+   color: Colours.app_sub_white,
+   fontWeight: FontWeight.w300,
+   fontFamily: fontName,
+ );
+
+ static const TextStyle textBoldSubBlack24 = TextStyle(
+   fontSize: Dimens.font_sp24,
+   color: Colours.app_sub_black,
+   fontWeight: FontWeight.w700,
+   fontFamily: fontName,
+ );
+
+ static const TextStyle textBoldSubBlacke20 = TextStyle(
+   fontSize: Dimens.font_sp20,
+   color: Colours.app_sub_black,
+   fontWeight: FontWeight.w700,
+   fontFamily: fontName,
+ );
+
+ static const TextStyle textLightSubBlack18 = TextStyle(
+   fontSize: Dimens.font_sp18,
+   color: Colours.app_sub_black,
+   fontWeight: FontWeight.w300,
+   fontFamily: fontName,
+ );
+
+ static const TextStyle textBoldSubBlack18 = TextStyle(
+   fontSize: Dimens.font_sp18,
+   color: Colours.app_sub_black,
+   fontWeight: FontWeight.w700,
+   fontFamily: fontName,
+ );
+ static const TextStyle textRegularSubBlack16 = TextStyle(
+   fontSize: Dimens.font_sp16,
+   color: Colours.app_sub_black,
+   fontWeight: FontWeight.w400,
+   fontFamily: fontName,
+ );
+
+ static const TextStyle textLightSubBlack16 = TextStyle(
+   fontSize: Dimens.font_sp16,
+   color: Colours.app_sub_black,
+   fontWeight: FontWeight.w300,
+   fontFamily: fontName,
+ );
+
+ static const TextStyle textBoldSubBlack12 = TextStyle(
+   fontSize: Dimens.font_sp12,
+   color: Colours.app_sub_black,
+   fontWeight: FontWeight.w700,
+   fontFamily: fontName,
+ );
+
+ static const TextStyle textLightSubBlack12 = TextStyle(
+   fontSize: Dimens.font_sp12,
+   color: Colours.app_sub_black,
+   fontWeight: FontWeight.w300,
+   fontFamily: fontName,
+ );
+
+ static const TextStyle textBoldSubBlack10 = TextStyle(
+   fontSize: Dimens.font_sp10,
+   color: Colours.app_sub_black,
+   fontWeight: FontWeight.w700,
+   fontFamily: fontName,
+ );
+
+ static const TextStyle textLightSubBlack10 = TextStyle(
+   fontSize: Dimens.font_sp10,
+   color: Colours.app_sub_black,
+   fontWeight: FontWeight.w300,
+   fontFamily: fontName,
+ );
+
+ static const TextStyle textLightMain10 = TextStyle(
+   fontSize: Dimens.font_sp10,
+   color: Colours.app_main,
+   fontWeight: FontWeight.w300,
+   fontFamily: fontName,
+ );
+
+ static const TextStyle textBoldMain10 = TextStyle(
+   fontSize: Dimens.font_sp10,
+   color: Colours.app_main,
+   fontWeight: FontWeight.w700,
+   fontFamily: fontName,
+ );
+
+ static const TextStyle textLightSubGreen10 = TextStyle(
+   fontSize: Dimens.font_sp10,
+   color: Colours.app_sub_green,
+   fontWeight: FontWeight.w300,
+   fontFamily: fontName,
+ );
+
+ static const TextStyle textLightSubGrey10 = TextStyle(
+   fontSize: Dimens.font_sp10,
+   color: Colours.app_sub_grey,
+   fontWeight: FontWeight.w300,
+   fontFamily: fontName,
+ );
+
+ static const TextStyle textLightSubGrey12 = TextStyle(
+   fontSize: Dimens.font_sp12,
+   color: Colours.app_sub_grey,
+   fontWeight: FontWeight.w300,
+   fontFamily: fontName,
+ );
+}
