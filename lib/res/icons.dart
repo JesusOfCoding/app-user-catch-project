e532dfb991ec6aae0a79c9648f7de93af731221e
heesun <heesun9793@naver.com>
2023-04-15 23:02:20 +0900

    ic아이콘 추가 중-2

--- lib/res/icons.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/cupertino.dart';
-
-class Icons {
-  static const search = Icon(CupertinoIcons.search);
-  static const infoMark = Icon(CupertinoIcons.)
-}
---
+++ lib/res/my_icons.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/cupertino.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class MyIcons {
+  static const search = Icon(CupertinoIcons.search);
+  static const notification = Icon(CupertinoIcons.bell);
+  static const scrap = Icon(CupertinoIcons.bookmark);
+  static const scrapFill = Icon(CupertinoIcons.bookmark_fill);
+  static const heart = Icon(CupertinoIcons.heart);
+  static const heartFill = Icon(CupertinoIcons.heart_fill);
+  static const paperplain = Icon(CupertinoIcons.paperplane_fill);
+  static const home = Icon(CupertinoIcons.house);
+  static const home_fill = Icon(CupertinoIcons.house_fill);
+  static const review = Icon(CupertinoIcons.bubble_right);
+  static const filter = Icon(CupertinoIcons.slider_horizontal_3);
+  static const creditCard = Icon(CupertinoIcons.creditcard_fill);
+  static const profileCircle = Icon(CupertinoIcons.person_circle);
+  static const calenderList = Icon(CupertinoIcons.calendar_circle);
+  static const calenderListFill = Icon(CupertinoIcons.calendar_circle_fill);
+  static const myPage = Icon(CupertinoIcons.person);
+  static const star = Icon(CupertinoIcons.star_fill);
+  static const call = Icon(CupertinoIcons.phone);
+  static const sortDown = Icon(CupertinoIcons.sort_down);
+  static const timeCircle = Icon(CupertinoIcons.time);
+  static const arrowUp = Icon(CupertinoIcons.arrow_up);
+  static const resetFilter = Icon(CupertinoIcons.arrow_2_circlepath);
+  static const reset = Icon(CupertinoIcons.arrow_2_circlepath);
+  static const mapPlaceMark = Icon(CupertinoIcons.placemark_fill);
+  static const checkMark = Icon(CupertinoIcons.check_mark);
+  static const sun = Icon(CupertinoIcons.sun_max_fill);
+  static const moon = Icon(CupertinoIcons.moon_fill);
+  static const pencilCircle = Icon(CupertinoIcons.pencil_circle);
+  static const mapMark = Icon(CupertinoIcons.map);
+  static const infoMark = Icon(CupertinoIcons.info);
+  static const questionMark = Icon(CupertinoIcons.question_circle);
+  static const arrowDown = Icon(CupertinoIcons.chevron_down);
+  static const arrowRight = Icon(CupertinoIcons.chevron_forward);
+  static const more = Icon(CupertinoIcons.ellipsis);
+  static const setting = Icon(CupertinoIcons.gear);
+  static const parkingCircle = Icon(FontAwesomeIcons.productHunt);
+  static const parkingSquare = Icon(FontAwesomeIcons.squareParking);
+  static const share = Icon(FontAwesomeIcons.shareNodes);
+  static const window = Icon(FontAwesomeIcons.borderAll);
+  static const won = Icon(FontAwesomeIcons.wonSign);
+  static const forkKnife = Icon(FontAwesomeIcons.utensils);
+  static const con = Icon(FontAwesomeIcons.utensils);
+  static const comfortable = Icon(CupertinoIcons.rectangle_grid_2x2);
+  static const cancelAndPayback = Icon(CupertinoIcons.folder_fill_badge_minus);
+  static const wine = Icon(FontAwesomeIcons.wineGlass);
+
+
+}
